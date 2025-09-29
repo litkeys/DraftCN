1cddfab5780bb8a2af09105a2b4b41e3c8baa99d
Ninefigurelegend <ninefigurelegend@gmail.com>
2025-09-30 09:08:31 +1000

    Added favicon and logo
    Ready to launch

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import { ZoomControl } from './ZoomControl'
 import { ImportButton } from './ImportButton'
 import { ExportButton } from './ExportButton'
@@ -7,7 +8,13 @@ export function Header() {
   return (
     <header className="h-16 bg-background border-b border-border flex items-center px-6 relative">
       <div className="flex items-center flex-1">
-        <h1 className="text-xl font-semibold text-foreground">DraftCN</h1>
+        <Image
+          src="/draftcn-logo.png"
+          alt="DraftCN Logo"
+          width={32}
+          height={32}
+          className="rounded-sm -translate-x-2.5"
+        />
       </div>
       <div className="absolute left-1/2 transform -translate-x-1/2">
         <ZoomControl />
